f3102f9aa86e20f65baba0506856c22adb93d123
Isaac Oluwafemi Ogunniyi <isaacogunniyi1@gmail.com>
2023-05-23 00:17:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import pickle
 
 
 def train_model(model,train,target):
-    X = drop.drop('is_fraud',axis=1)
-    y = data['is_fraud']
+    X = train.drop(tardet,axis=1)
+    y = train[target]
     X_train,X_test,y_train,y_test= sklearn.model_selection.train_test_split(X,
     y,test_size=0.3,random_state=2)
     model = model.fit(X_train,y_train)
